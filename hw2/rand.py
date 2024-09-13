147cc8d9e9b020c8c6f0ca8eaedbd89b97e19a0c
Firasat Hussain Mohammed <mfirasathussain@gmail.com>
2024-09-12 22:33:51 -0400

    test commit to check worflow badges

--- hw2/rand.py
+++ hw2/rand.py
@@ -2,17 +2,16 @@
 This module provides utilities for generating random arrays.
 """
 import random
-
 def random_array(arr):
     """
     Fills an array with random numbers using Python's random module.
 
     Args:
         arr (list): An empty list that will be filled with random numbers.
-
     Returns:
         list: The array filled with random integers.
     """
     for i, _ in enumerate(arr):
-        arr[i] = random.randint(1, 20)  # Generates a random integer between 1 and 20
+        # Generates a random integer between 1 and 20
+        arr[i] = random.randint(1, 20)
     return arr
